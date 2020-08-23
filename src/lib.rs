ed506a190467db3e469282e03a6293e0c7031f18
zhiluniao <huangshunle_gxu@163.com>
2020-08-24 00:18:21 +0800

    add write_post.rs

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ extern crate diesel;
 use diesel::prelude::*;
 use dotenv::dotenv;
 use std::env;
+use self::models::{Post,NewPost};
 
 pub fn establish_connection() -> MysqlConnection {
     dotenv().ok();
@@ -14,4 +15,17 @@ pub fn establish_connection() -> MysqlConnection {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     MysqlConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
+pub fn create_post(conn: &MysqlConnection, title: &str, body: &str) -> Post {
+    use crate::schema::posts;
+
+    let new_post = NewPost { title, body };
+
+    diesel::insert_into(posts::table)
+        .values(&new_post)
+        .execute(conn)
+        .expect("Error saving new post");
+
+    posts::table.order(posts::id.desc()).first(conn).unwrap()
 }
--- src/models.rs
+++ src/models.rs
@@ -1,4 +1,5 @@
 use diesel::Queryable;
+use super::schema::posts;
 
 #[derive(Queryable)]
 pub struct Post {
@@ -6,4 +7,11 @@ pub struct Post {
     pub title: String,
     pub body: String,
     pub published: bool,
+}
+
+#[derive(Insertable)]
+#[table_name="posts"]
+pub struct NewPost<'a> {
+    pub title: &'a str,
+    pub body: &'a str,
 }
